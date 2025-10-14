68f3851a032c00485f7f69cd978b4e3af0bba340
Yutkar <kira20061710@gmail.com>
2025-10-14 22:10:08 +0500

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -6,7 +6,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
 
 const firebaseConfig = {
   apiKey: "AIzaSyAidL-8CTSIQw-wkiV_puD-DS-dQWX5u0Q",
-  authDomain: "union-heart-games.firebaseapp.com",
+  authDomain: "yutkar.github.io",
   projectId: "union-heart-games",
   storageBucket: "union-heart-games.firebasestorage.app",
   messagingSenderId: "56294430695",
